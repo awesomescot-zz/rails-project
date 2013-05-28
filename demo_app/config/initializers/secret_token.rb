5fe3b8e12e497383b259bd581315cde042c62d54
Scot Spinner <scot.spinner@locusenergy.com>
2013-05-28 14:16:16 -0700

    initial commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9ed51937f707d69c0a0e96cf06a15171bba6be4b8095d6e30f1cfe42e559b89ce5c56a3101a39c1e29c282f9f66adcb71b1a44e537d3db4de59a2a1daf1d936e'
