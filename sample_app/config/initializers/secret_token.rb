341eea0b8167679a38a99da436fbf731e00290f1
Scot Spinner <scot.spinner@locusenergy.com>
2013-05-31 17:05:31 -0700

    finished chapter 3

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '890426be1b0c9a2a323a75d8cb29e975f32db8212c57fa94fb5db8e18754988c26a9392bdf0211db3ec57a31d8819fab5203efd5e0526c9af4a1627700b4eec0'
